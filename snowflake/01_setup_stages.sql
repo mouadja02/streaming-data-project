4508e00b657cb3e3e6e185e9d7929ab5d082b2b1
Mouad Jaouhari <mouadpro02@gmail.com>
2025-06-25 01:56:52 +0200

    solving env credentials in snowflake sql

--- snowflake/01_setup_stages.sql
+++ snowflake/01_setup_stages.sql
@@ -30,38 +30,37 @@ CREATE OR REPLACE STAGE iceberg_stage
 
 -- Set AWS credentials (replace with your actual credentials)
 -- You can also set these as Snowflake variables or use environment variables
-SET (YOUR_ACCESS_KEY_ID, YOUR_SECRET_ACCESS_KEY) = ('${AWS_ACCESS_KEY_ID}', '${AWS_SECRET_ACCESS_KEY}');
-
 -- Create database and schema if they don't exist
+
 CREATE DATABASE IF NOT EXISTS ECOMMERCE_DB;
 USE DATABASE ECOMMERCE_DB;
 CREATE SCHEMA IF NOT EXISTS STAGING;
 USE SCHEMA STAGING;
 
 -- Create stages for each data type
-CREATE STAGE IF NOT EXISTS raw_users_stage
+CREATE OR REPLACE STAGE raw_users_stage
   URL = 's3://my-amazing-app/users/raw/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = $YOUR_ACCESS_KEY_ID
-    AWS_SECRET_KEY = $YOUR_SECRET_ACCESS_KEY
+    AWS_KEY_ID = ${YOUR_ACCESS_KEY_ID}
+    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for raw user data from Kafka pipeline';
 
-CREATE STAGE IF NOT EXISTS user_analytics_stage
+CREATE OR REPLACE STAGE user_analytics_stage
   URL = 's3://my-amazing-app/users/analytics/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = $YOUR_ACCESS_KEY_ID 
-    AWS_SECRET_KEY = $YOUR_SECRET_ACCESS_KEY
+    AWS_KEY_ID = ${YOUR_ACCESS_KEY_ID} 
+    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for user analytics data (gender, email domains, etc.)';
 
-CREATE STAGE IF NOT EXISTS user_demographics_stage
+CREATE OR REPLACE STAGE user_demographics_stage
   URL = 's3://my-amazing-app/users/demographics/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = $YOUR_ACCESS_KEY_ID
-    AWS_SECRET_KEY = $YOUR_SECRET_ACCESS_KEY
+    AWS_KEY_ID = ${YOUR_ACCESS_KEY_ID}
+    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for user demographics data (countries, states)';
