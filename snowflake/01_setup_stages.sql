bd9b69459912f4ae4dd986b6a5a9410bb96a53f8
Mouad Jaouhari <mouadpro02@gmail.com>
2025-06-25 02:12:35 +0200

    solvingexternal tables locations in snowflake sql

--- snowflake/01_setup_stages.sql
+++ snowflake/01_setup_stages.sql
@@ -41,8 +41,8 @@ USE SCHEMA STAGING;
 CREATE OR REPLACE STAGE raw_users_stage
   URL = 's3://my-amazing-app/users/raw/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = ${AWS_ACCESS_KEY_ID}
-    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
+    AWS_KEY_ID = '${AWS_ACCESS_KEY_ID}'
+    AWS_SECRET_KEY = '${AWS_SECRET_ACCESS_KEY}'
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for raw user data from Kafka pipeline';
@@ -50,8 +50,8 @@ CREATE OR REPLACE STAGE raw_users_stage
 CREATE OR REPLACE STAGE user_analytics_stage
   URL = 's3://my-amazing-app/users/analytics/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = ${AWS_ACCESS_KEY_ID} 
-    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
+    AWS_KEY_ID = '${AWS_ACCESS_KEY_ID}' 
+    AWS_SECRET_KEY = '${AWS_SECRET_ACCESS_KEY}'
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for user analytics data (gender, email domains, etc.)';
@@ -59,8 +59,8 @@ CREATE OR REPLACE STAGE user_analytics_stage
 CREATE OR REPLACE STAGE user_demographics_stage
   URL = 's3://my-amazing-app/users/demographics/parquet/'
   CREDENTIALS = (
-    AWS_KEY_ID = ${AWS_ACCESS_KEY_ID}
-    AWS_SECRET_KEY = ${AWS_SECRET_ACCESS_KEY}
+    AWS_KEY_ID = '${AWS_ACCESS_KEY_ID}'
+    AWS_SECRET_KEY = '${AWS_SECRET_ACCESS_KEY}'
   )
   FILE_FORMAT = (TYPE = 'PARQUET' COMPRESSION = 'SNAPPY')
   COMMENT = 'Stage for user demographics data (countries, states)';
